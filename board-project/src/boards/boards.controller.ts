2c8b4a96d890989bf0faaf2cf11b81d898a589bf
schoe <schoe@student.42seoul.kr>
2023-02-24 20:16:40 +0900

    1

---
+++ board-project/src/boards/boards.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService){}
+
+  @Get()
+  getAllBoard() : Board[] {
+    return this.boardsService.getAllBoards();
+  }
+}
---
+++ board-project/src/boards/boards.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid} from 'uuid';
+@Injectable()
+export class BoardsService {
+  private boards : Board[] = [];
+
+  getAllBoards() : Board[] {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string){
+    const board: Board = {
+      id : uuid(),
+      title : title,
+      description : description,
+      status : BoardStatus.PUBLIC
+    }
+
+    this.boards.push(board);
+    return board;
+  }
+}
